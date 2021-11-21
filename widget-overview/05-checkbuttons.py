ed6d720e85e6597a537b0b50feb141daed1f5c70
Sakis Tosounidis <sakis.tosounidis@gmail.com>
2021-11-21 14:33:53 +0200

    Chapters 5,6 and 7

---
+++ widget-overview/05-checkbuttons.py
@@ -0,0 +1,34 @@
+import tkinter as tk 
+from tkinter import ttk 
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+# check_button = ttk.Checkbutton(root, text="Check me!")
+# check_button.pack()
+
+# check_button["state"] = "disabled"
+
+selected_option = tk.StringVar()
+
+def print_current_option():
+    print(selected_option.get())
+
+check = ttk.Checkbutton(
+    root,
+    text="Check example",
+    variable=selected_option,
+    command=print_current_option,
+    onvalue="On",
+    offvalue="Off"
+)
+
+check.pack()
+
+root.mainloop()
