861c87e9feeb00b2a48a62f6381cb9fb679e2ad9
Sakis Tosounidis <sakis.tosounidis@gmail.com>
2021-11-21 12:44:34 +0200

    Chapter 2: text widget

--- widget-overview/01-labels.py
+++ widget-overview/01-labels.py
@@ -1,15 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
+from windows import set_dpi_awareness
 
+set_dpi_awareness()
 
 root = tk.Tk()
 root.geometry("600x400")
 root.resizable(False, False)
 root.title("Widget Examples")
 
+# label = ttk.Label(root, text="Hello World", padding=20)
+# label.config(font=("Segoe UI", 20))
+# label.pack()
 
+# image = Image.open("logo.png").resize((64,64))
+# photo = ImageTk.PhotoImage(image)
+# label = ttk.Label(root, text="Image with text", image=photo, padding=5, compound="right")
+# label.pack()
 
+greeting = tk.StringVar()
 
+label = ttk.Label(root, padding=10, textvariable=greeting)
+label.pack()
 
+greeting.set("Hello, John!")
 
 root.mainloop()
---
+++ widget-overview/02-text-widgets.py
@@ -0,0 +1,23 @@
+import tkinter as tk 
+from tkinter import ttk 
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+text = tk.Text(root, height=8)
+text.pack()
+
+text.insert("1.0", "Please enter a comment...")
+
+# text["state"] = "disabled"
+# counterpart to 'disabled' is 'normal' !
+
+text_content = text.get("1.0", "end")
+print(text_content)
+
+root.mainloop()
