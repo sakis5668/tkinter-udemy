3da9b4287134bb548fa5a62b602d671ee6ba1466
Sakis Tosounidis <sakis.tosounidis@gmail.com>
2021-11-21 12:59:18 +0200

    Chapters 3 and 4

---
+++ widget-overview/04-separators.py
@@ -0,0 +1,21 @@
+import tkinter as tk 
+from tkinter import ttk 
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+
+ttk.Label(root, text="Hello, world!", padding=20).pack()
+
+ttk.Separator(root, orient="horizontal").pack(fill="x")
+
+ttk.Label(root, text="Hello, world!", padding=20).pack()
+
+root.mainloop()
